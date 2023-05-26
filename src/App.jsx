703f31f24cda9173b9fe3b521c98723f6c4d7890
Amirparsa Naghibi <amirparsanaghibi@gmail.com>
2023-05-26 23:59:22 +0330

    Making the project structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// styles
+import "./App.css";
+
+const App = () => {
+    return (
+        <div>
+           <h1 className='text-xl underline text-blue-700'>App</h1>
+
+           <span className='border-spacing-1 text-red-700'>test</span>
+        </div>
+    );
+};
+
+export default App;
