b64dd6ee9bdced9232343f85f56b225c7f5e2a0b
Amirparsa Naghibi <amirparsanaghibi@gmail.com>
2023-06-02 22:13:56 +0330

    Creating an order page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,18 +1,35 @@
 import React from 'react'
 
 import { ColumnDirective, ColumnsDirective, Filter, GridComponent, Group } from '@syncfusion/ej2-react-grids';
-import { Inject, Page, Sort } from '@syncfusion/ej2-react-grids';
+import { Inject, Page, Sort, Resize,ContextMenu } from '@syncfusion/ej2-react-grids';
 
 // Data
 import { ordersData, contextMenuItems, ordersGrid } from '../data/Data'
 
 import {Header} from '../components/export'
 
+
+
 const Orders = () => {
   return (
-    <div className='m-4 md:mb-10 p-2 md:p-10 bg-white rounded-3xl'>
+    <div className='m-4 mt-36 sm:mt-0 md:mb-10 p-2 md:p-10 bg-white rounded-3xl'>
 
       <Header category={"Page"} title={"Orders"} />
+
+      <GridComponent
+        id='gridComp'
+        dataSource={ordersData}
+        allowPaging={true}
+        allowSorting={true}
+      >
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+
+        <Inject services={[Page, Sort, Resize, Filter, Group,ContextMenu]}/>
+      </GridComponent>
 
     </div>
   )
