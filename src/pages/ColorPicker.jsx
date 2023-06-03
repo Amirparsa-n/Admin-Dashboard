3bc21bdd34cf0b283b5562bd316cf989db36b675
Amirparsa Naghibi <amirparsanaghibi@gmail.com>
2023-06-03 18:23:01 +0330

    create a Color Picker section

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
 
+import { ColorPickerComponent } from '@syncfusion/ej2-react-inputs';
+
+
+// Data
+import { EditorData } from '../data/Data';
+
+// Component
+import {Header} from '../components/export'
+
 const ColorPicker = () => {
+
+  const change = (args) => {
+    document.getElementById('preview').style.backgroundColor = args.currentValue.hex;
+  }
+
   return (
-    <div>ColorPicker</div>
+    <div className='m-4 mt-36 sm:mt-0 md:mb-10 p-2 md:p-10 bg-white rounded-3xl'>
+
+      <Header category={"Apps"} title={"ColorPicker"} />
+
+      <div className='text-center'>
+        <div id="preview" className='w-80 md:w-800'/>
+  
+        <div className="flex justify-center items-center gap-20 flex-wrap">
+              <div>
+                <p className='text-2xl font-semibold mt-2 mb-4'>Color Palette</p>
+                <ColorPickerComponent  id='inline-palette' mode='Palette' modeSwitcher={false} inline={true} showButtons={false} change={change}/>
+              </div>
+              <div>
+                <p className='text-2xl font-semibold mt-2 mb-4'>Color Picker</p>
+                <ColorPickerComponent  id='Picker' mode='Picker' modeSwitcher={false} inline={true} showButtons={false} change={change}/>
+              </div>
+          </div>
+      </div>
+
+
+    </div>
   )
 }
 
