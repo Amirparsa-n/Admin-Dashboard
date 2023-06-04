8f6b3569e24b9461809ae8640ebfddd29a89966e
Amirparsa Naghibi <amirparsanaghibi@gmail.com>
2023-06-05 02:58:28 +0330

    Making a Stacked chart page

--- src/pages/Charts/Stacked.jsx
+++ src/pages/Charts/Stacked.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
+// Component
+import { Stacked as StackedChart } from '../../components/export'
+
+// Component
+import {Header} from '../../components/export'
+
+// Context
+import { stateContext } from '../../contexts/ContextProvide';
+
+
 const Stacked = () => {
   return (
-    <div>Stacked</div>
+    <div className='m-4 mt-36 sm:mt-0 md:mb-10 p-2 md:p-10 bg-white rounded-3xl dark:bg-secondary-dark-bg transition-colors'>
+
+      <Header category={"Charts"} title={"Stacked"} />
+
+      <div className='w-full'>
+        <StackedChart />
+      </div>
+        
+    </div>
   )
 }
 
